5d3f60e8f687b93d69ee426830a919b22b30ec32
Sachiyo <sachiyology@gmail.com>
2026-03-15 01:34:21 +0900

    Implement Selenium BDD web step definitions

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -24,111 +24,92 @@ Steps file for web interactions with Selenium
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
+######################################################################
+# Web Steps for BDD UI Tests
+######################################################################
 import logging
-from behave import when, then
+from behave import given, when, then
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import Select
 
-ID_PREFIX = 'product_'
+logger = logging.getLogger(__name__)
 
-
+######################################################################
+# VISIT PAGES
+######################################################################
 @when('I visit the "Home Page"')
 def step_impl(context):
-    """ Make a call to the base URL """
-    context.driver.get(context.base_url)
-    # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
+    """Navigate to the home page"""
+    context.browser.get(context.base_url)
 
-@then('I should see "{message}" in the title')
-def step_impl(context, message):
-    """ Check the document title for a message """
-    assert(message in context.driver.title)
 
-@then('I should not see "{text_string}"')
-def step_impl(context, text_string):
-    element = context.driver.find_element(By.TAG_NAME, 'body')
-    assert(text_string not in element.text)
+######################################################################
+# PAGE VALIDATION
+######################################################################
+@then('I should see "{text}" in the title')
+def step_impl(context, text):
+    """Check the page title"""
+    assert text in context.browser.title
 
-@when('I set the "{element_name}" to "{text_string}"')
-def step_impl(context, element_name, text_string):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = context.driver.find_element(By.ID, element_id)
+
+@then('I should see "{text}" on the page')
+def step_impl(context, text):
+    """Check that text appears somewhere on the page"""
+    assert text in context.browser.page_source
+
+
+######################################################################
+# FORM INPUT
+######################################################################
+@when('I set the "{field}" to "{value}"')
+def step_impl(context, field, value):
+    """Enter text into an input field"""
+    element = context.browser.find_element(By.NAME, field)
     element.clear()
-    element.send_keys(text_string)
+    element.send_keys(value)
 
-@when('I select "{text}" in the "{element_name}" dropdown')
-def step_impl(context, text, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = Select(context.driver.find_element(By.ID, element_id))
-    element.select_by_visible_text(text)
 
-@then('I should see "{text}" in the "{element_name}" dropdown')
-def step_impl(context, text, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = Select(context.driver.find_element(By.ID, element_id))
-    assert(element.first_selected_option.text == text)
+######################################################################
+# BUTTON ACTION
+######################################################################
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    """Click a button"""
+    btn = context.browser.find_element(By.XPATH, f"//button[text()='{button}']")
+    btn.click()
 
-@then('the "{element_name}" field should be empty')
-def step_impl(context, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = context.driver.find_element(By.ID, element_id)
-    assert(element.get_attribute('value') == u'')
 
-##################################################################
-# These two function simulate copy and paste
-##################################################################
-@when('I copy the "{element_name}" field')
-def step_impl(context, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.presence_of_element_located((By.ID, element_id))
-    )
-    context.clipboard = element.get_attribute('value')
-    logging.info('Clipboard contains: %s', context.clipboard)
+######################################################################
+# DROPDOWN SELECTION
+######################################################################
+@when('I select "{value}" in the "{field}" dropdown')
+def step_impl(context, value, field):
+    """Select value from dropdown"""
+    select = Select(context.browser.find_element(By.NAME, field))
+    select.select_by_visible_text(value)
 
-@when('I paste the "{element_name}" field')
-def step_impl(context, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.presence_of_element_located((By.ID, element_id))
-    )
-    element.clear()
-    element.send_keys(context.clipboard)
 
-##################################################################
-# This code works because of the following naming convention:
-# The buttons have an id in the html hat is the button text
-# in lowercase followed by '-btn' so the Clean button has an id of
-# id='clear-btn'. That allows us to lowercase the name and add '-btn'
-# to get the element id of any button
-##################################################################
+######################################################################
+# CHECKBOX
+######################################################################
+@when('I check "{field}"')
+def step_impl(context, field):
+    """Check a checkbox"""
+    checkbox = context.browser.find_element(By.NAME, field)
+    if not checkbox.is_selected():
+        checkbox.click()
 
-## UPDATE CODE HERE ##
 
-##################################################################
-# This code works because of the following naming convention:
-# The id field for text input in the html is the element name
-# prefixed by ID_PREFIX so the Name field has an id='pet_name'
-# We can then lowercase the name and prefix with pet_ to get the id
-##################################################################
+######################################################################
+# RESULTS VALIDATION
+######################################################################
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    """Verify item appears in results"""
+    assert name in context.browser.page_source
 
-@then('I should see "{text_string}" in the "{element_name}" field')
-def step_impl(context, text_string, element_name):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.text_to_be_present_in_element_value(
-            (By.ID, element_id),
-            text_string
-        )
-    )
-    assert(found)
 
-@when('I change "{element_name}" to "{text_string}"')
-def step_impl(context, element_name, text_string):
-    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
-    element = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.presence_of_element_located((By.ID, element_id))
-    )
-    element.clear()
-    element.send_keys(text_string)
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    """Verify item does not appear in results"""
+    assert name not in context.browser.page_source
